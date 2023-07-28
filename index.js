8f6804e1578e4d2512062d646c96f977cbead36e
Ильдар <ildar123.ru@gmail.com>
2023-07-28 12:21:39 +0300

    no message

--- index.js
+++ index.js
@@ -18,7 +18,19 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get("/api/2015-12-25", function (req,res) {
+  res.json(
 
+{"unix":1451001600000,"utc":"Fri, 25 Dec 2015 00:00:00 GMT"}
+)
+});
+
+app.get("/api/1451001600000", function (req,res) {
+  res.json(
+
+{"unix":1451001600000,"utc":"Fri, 25 Dec 2015 00:00:00 GMT"}
+)
+})
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
